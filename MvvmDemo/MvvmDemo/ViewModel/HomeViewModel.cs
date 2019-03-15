3d6683a6ccf8efa3fefcb53eefc3c1529651b26a
Abhishek <abhishabd1@outlook.com>
2019-03-15 17:20:08 +0530

    Initial commit
    Initial commit

---
+++ MvvmDemo/MvvmDemo/ViewModel/HomeViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MvvmDemo.ViewModel
+{
+    public class HomeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public HomeViewModel()
+        {
+            CheckDate = new Command(Check);
+        }
+        private DateTime selecteddate { get; set; }
+
+        public DateTime SelectedDate
+        {
+            get { return selecteddate; }
+            set
+            {
+                selecteddate = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("SelectedDate"));
+            }
+        }
+        private string totalage { get; set; }
+        public string TotalAge
+        {
+            get { return totalage; }
+            set
+            {
+                totalage = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("TotalAge"));
+            }
+        }
+        public ICommand CheckDate { protected set; get; }
+
+
+
+        public void Check()
+        {
+            DateTime currentdate = DateTime.Now;
+            var totlaldays = (currentdate - selecteddate).TotalDays;
+
+            var totalYears = Math.Truncate(totlaldays / 365);
+            var totalMonth = Math.Truncate((totlaldays % 365) / 30);
+            var remaningday = Math.Truncate((totlaldays % 365) % 30);
+            TotalAge = "Year :" + totalYears + " Month: " + totalMonth + " Day :" + remaningday;
+
+
+
+        }
+
+    }
+}
